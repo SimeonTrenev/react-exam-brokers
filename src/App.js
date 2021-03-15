370c98f6f66053c0db8ba56dfcd949a3c3be1f73
Simeon Trenev <65025840+SimeonTrenev@users.noreply.github.com>
2021-03-15 17:00:13 +0200

    add components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import AddOptions from './components/addOptions'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AddOptions />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/addOptions.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+
+class AddOptions extends  Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return(
+            <div>
+                <form>
+                    
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddOptions

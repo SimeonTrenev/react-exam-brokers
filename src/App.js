f808237bd8bc1ddccfe48a9016634746396c4fed
Simeon Trenev <65025840+SimeonTrenev@users.noreply.github.com>
2021-03-16 11:14:50 +0200

    add home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AddOptions from './components/addOptions'
+import HomePage from './components/homePage'
 import './App.css';
 
 function App() {
   return (
     <div >
+      <HomePage />
       <AddOptions />
     </div>
   );

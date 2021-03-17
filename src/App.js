0d7aaf1753b64057056962d5bfcc33f15d19ce86
Simeon Trenev <65025840+SimeonTrenev@users.noreply.github.com>
2021-03-17 11:48:04 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import HomePage from './components/homePage'
 import AddOffer from './components/addOffer'
 import OffersFilter from './components/OffersFilters'
 import './App.css';
+import { Route, Link, NavLink, Redirect, Switch, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div >
-      <AddOffer />
-      <HomePage />
-      <AddOptions />
-      <OffersFilter />
+      <BrowserRouter>
+      <Switch>
+        <Route path='/' exact={true}>
+        <HomePage />
+        </Route>
+        <Route path='/add-offer' component={AddOffer} />
+      <Route path='/add-options' component={AddOptions}/>
+      <Route path='/show-offers' component={OffersFilter} />
+      
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
